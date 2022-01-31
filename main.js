546438b8de7356b5b79c946ffcc7bcdd91b46774
hamzasolicode <alaoui.hamza.solicode@gmail.com>
2022-01-31 12:41:49 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,49 @@
+
+       $(document).ready(function(){                            //link jquery with fichier json
+        $.getJSON("produits.json", function(data){
+         var tab = '';
+         
+         $.each(data, function(key, value){
+             tab +=  '<tr>';
+             tab +=  '<td>'+value.id+'</td>';
+             tab +=  '<td>'+value.Brands_Name+'</td>';
+             tab +=  '<td>'+value.Price+'</td>';
+             tab +=  '<td>'+value.A_Category+'</td>';
+             tab +=  '<td>'+value.Availability+'</td>';
+             tab +=  '<td>'+"RaisonSocial: "+value.A_supplier.raisonSocial+'<br>'+"Adress: "+value.A_supplier.adress+'</td>';
+             tab +=  '</tr>';
+         });
+         $('#tablee').append(tab);
+        });
+    });
+
+    $(document).ready(function(){                                   //for recherche 
+        $("#search").on("keyup",function(){
+    var value = $(this).val().toLowerCase();
+
+        $("#tBody tr").filter(function(){
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1  )
+        });
+        
+       } );
+
+    });
+
+//sort
+
+    $('th').click(function(){
+      var table = $(this).parents('table').eq(0)
+      var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()))
+      this.asc = !this.asc
+      if (!this.asc){rows = rows.reverse()}
+      for (var i = 0; i < rows.length; i++){table.append(rows[i])}
+  })
+
+  
+  function comparer(index) {
+      return function(a, b) {
+          var valA = getCellValue(a, index), valB = getCellValue(b, index)
+          return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.toString().localeCompare(valB)
+      }
+  }
+  function getCellValue(row, index){ return $(row).children('td').eq(index).text() }
